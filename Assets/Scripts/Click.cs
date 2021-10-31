0f2746a7a414179783b959afbcc2a307edb64247
Ilia6667 <90097815+Ilia6667@users.noreply.github.com>
2021-10-31 23:05:41 +0600

    GameGun
    GameGun

---
+++ Assets/Scripts/Click.cs
@@ -0,0 +1,23 @@
+﻿
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Click : MonoBehaviour, IPointerClickHandler
+{
+    public void OnPointerClick(PointerEventData pointerEventDada)
+    {
+        Application.LoadLevel(1);
+    }
+
+
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        
+    }
+}
